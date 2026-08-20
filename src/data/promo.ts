82c1393151a43bee413e94bee624e25eacdb8fb0
Dimas Okva <official.jaripos@gmail.com>
2026-08-20 10:34:49 +0700

    Update Promo

--- src/data/promo.ts
+++ src/data/promo.ts
@@ -3,5 +3,5 @@ export const promoKemerdekaan = {
   title: "MERDEKA SALE 50%",
   validUntil: "2026-08-24T23:59:59",
   scarcityText: "Terbatas! Hanya untuk 100 Pendaftar Pertama",
-  fakeProgressPercentage: 24,
+  fakeProgressPercentage: 78,
 };
